805c294f064b4146d91409329bc6a6bddc8fd486
cocomong11 <carpediem1011@naver.com>
2025-11-13 15:46:26 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,17 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void){
-	int i;
-	int grade[5];
-	int sum,average;
-	
-	for(i=0;i<5;i++){
-		printf("grade[%i]=",i);
-		scanf("%d",&grade[i]);
-	}
-	for (i=0;i<5;i++){
-		sum+=grade[i];
-	}
-	average=sum/5;
-	printf("average=%d",average);
+	int i=100;
+	int *p=&i;
+	int **q=&p;
+	*p=200;
+	printf("i=%d,*p=%d,**q=%d\n",i,*p,**q);
+	**q=300;
+	printf("i=%d,*p=%d,**q=%d\n",i,*p,**q);
+	return 0;
 }
